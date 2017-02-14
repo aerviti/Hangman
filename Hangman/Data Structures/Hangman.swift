016ea86f5b0304aae111d403335c38dd62d76397
Alex Erviti <alexerviti@gmail.com>
2017-02-14 00:11:05 -0800

    Initial commit

---
+++ Hangman/Data Structures/Hangman.swift
@@ -0,0 +1,38 @@
+//
+//  Hangman.swift
+//  Hangman
+//
+//  Created by Alex Erviti on 2/13/17.
+//  Copyright © 2017 Alejandro Erviti. All rights reserved.
+//
+
+import Foundation
+
+//
+// Class used to
+//
+//
+
+class Hangman : NSObject, NSCoding {
+    
+    // MARK: - Enumerations
+    
+    enum Difficulty {
+        case Easy;
+        case Medium;
+        case Hard;
+    }
+    
+    // MARK: - Properties
+    
+    var difficulty: Int;
+    var numOfGuesses: Int;
+    
+    
+    
+    // MARK: - Initialization
+    
+    
+    
+    
+}
---
+++ Hangman/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  Hangman
+//
+//  Created by Alex Erviti on 2/13/17.
+//  Copyright © 2017 Alejandro Erviti. All rights reserved.
+//
+
+import Foundation
