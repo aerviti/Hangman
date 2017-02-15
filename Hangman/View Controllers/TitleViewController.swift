f72d31e1a4ff0a0e44f44c3d8458be431eef1baf
Alex Erviti <alexerviti@gmail.com>
2017-02-15 12:27:40 -0800

    Created files for view controllers and updated storyboard

---
+++ Hangman/View Controllers/TitleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TitleViewController.swift
+//  Hangman
+//
+//  Created by Alex Erviti on 2/13/17.
+//  Copyright © 2017 Alejandro Erviti. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+
+}
+
